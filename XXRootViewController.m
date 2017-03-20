9feeb542beef471862c10fb7842325dd66093f7b
ximmyxiao <xiaoxiao@xiaomatoiMac.local>
2017-03-20 21:51:46 +0800

    add socket connect

--- XXRootViewController.m
+++ XXRootViewController.m
@@ -1,9 +1,13 @@
 #import "XXRootViewController.h"
 #import "fmdb.h"
 #import "MsgCell.h"
+#import "GCDAsyncSocket.h"
 
-@interface XXRootViewController()
+@interface XXRootViewController()<GCDAsyncSocketDelegate>
 @property(nonatomic,strong) NSMutableArray* allMsgs;
+@property(nonatomic,strong) UITextField* iptf;
+@property(nonatomic,strong) UITextField* porttf;
+@property(nonatomic,strong) GCDAsyncSocket* socket;
 @end
 
 @implementation XXRootViewController {
@@ -20,6 +24,7 @@
 {
     NSMutableArray* thisTimeFetch = [NSMutableArray array];
 
+    
     NSString * path = @"/var/mobile/Library/SMS/sms.db";
     
     FMDatabase *db = [FMDatabase databaseWithPath:path];
@@ -58,6 +63,12 @@
     [rs close];
     
     [db close];
+    
+//    MsgModel* model = [MsgModel new];
+//    model.msgsender = @"1312424242";
+//    model.msgContent = @"sfdjaslfdjlaksfjkasjfdkasjfa";
+//    model.msgTime = @"2017-03-17 19:00:00";
+//    [thisTimeFetch addObject:model];
     self.allMsgs = thisTimeFetch;
     
 }
@@ -69,10 +80,26 @@
 }
 
 
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     [self.tableView registerClass:[MsgCell class] forCellReuseIdentifier:@"MsgCell"];
+    
+    UIView* headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 44)];
+    headerView.backgroundColor = [UIColor lightGrayColor];
+    
+    self.iptf = [[UITextField alloc] initWithFrame:CGRectMake(40, 12, 140, 20)];
+    self.iptf.backgroundColor = [UIColor whiteColor];
+    self.iptf.text = @"127.0.0.1";
+    [headerView addSubview:self.iptf];
+
+    self.porttf = [[UITextField alloc] initWithFrame:CGRectMake(200,12,100, 20)];
+    self.porttf.backgroundColor = [UIColor whiteColor];
+    self.porttf.text = @"9999";
+    [headerView addSubview:self.porttf];
+    
+    self.tableView.tableHeaderView = headerView;
 
     [self loadSMSData];
 }
@@ -104,6 +131,11 @@
     }
 }
 
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    [self.view endEditing:YES];
+}
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return [self.allMsgs count];
 }
@@ -124,6 +156,30 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
 	[tableView deselectRowAtIndexPath:indexPath animated:YES];
+    [self connectSocket];
 }
 
+
+#pragma mark - socket utility
+- (void)connectSocket
+{
+    self.socket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
+    NSError *error = nil;
+
+    if (![self.socket connectToHost:self.iptf.text onPort: [self.porttf.text integerValue]  error:nil])
+    {
+        UIAlertView* alert = [[UIAlertView alloc] initWithTitle:@"" message:[error description] delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [alert show];
+    }
+}
+
+- (void)socket:(GCDAsyncSocket *)sock didConnectToHost:(NSString *)host port:(UInt16)port
+{
+    NSString *requestStrFrmt = @"HEAD / HTTP/1.0\r\nHost: %@\r\n\r\n";
+    
+    NSString *requestStr = [NSString stringWithFormat:requestStrFrmt, @"127.0.0.1"];
+    NSData *requestData = [requestStr dataUsingEncoding:NSUTF8StringEncoding];
+    
+    [self.socket writeData:requestData withTimeout:-1.0 tag:0];
+}
 @end
